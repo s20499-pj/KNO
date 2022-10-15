d6b066f16a521b14108d929eed515481fe51bb80
Tomasz <ja.tomeasz@gmail.com>
2022-10-15 18:39:34 +0200

    First look to boardgame

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    value = None
+
+
+class Map:
+    empty = [[[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+             [[0, None, 0], [0, None, 0], [0, None, 0]],
+             [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
+
+    renderLine = ""
+
+    def print(self):
+        for x in range(len(self.empty)):
+            for y in range(len(self.empty[x])):
+                for z in range(len(self.empty[x][y])):
+                    self.renderLine += str(self.empty[x][y][z])
+                    if z != 2:
+                        if y == 0:
+                            self.renderLine += "-----"
+                        elif y == 1:
+                            self.renderLine += "---"
+                        else:
+                            self.renderLine += "-"
+                print(self.renderLine)
+                self.renderLine = ""
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    map = Map()
+    map.print()
+    
